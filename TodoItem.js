dc91291ff5c8aa776d184e7133aca61efe10abb9
AJ <amarayank4@gmail.com>
2020-10-13 10:38:47 +0530

    added TodoItem componenet

---
+++ TodoItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TodoItem(){
+  return (
+      <div className="todo-item">
+          <input type="checkbox" name="" id=""/>
+          <p>placeholder text here</p>
+      </div>
+  )   
+}
